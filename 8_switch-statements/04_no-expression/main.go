f2d44b9712203b0e82a9f45f4c8b13f9aadb8342
Tangellapalli, Phani <PTangellapal@mast.com>
2020-07-28 19:43:58 +0530

    switch statements

---
+++ 8_switch-statements/04_no-expression/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	myFriendsName := "Mar"
+
+	switch {
+	case len(myFriendsName) == 2:
+		fmt.Println("Wassup my friend with name of length 2")
+	case myFriendsName == "Tim":
+		fmt.Println("Wassup Tim")
+	case myFriendsName == "Jenny":
+		fmt.Println("Wassup Jenny")
+	case myFriendsName == "Marcus", myFriendsName == "Medhi":
+		fmt.Println("Your name is either Marcus or Medhi")
+	case myFriendsName == "Julian":
+		fmt.Println("Wassup Julian")
+	case myFriendsName == "Sushant":
+		fmt.Println("Wassup Sushant")
+	default:
+		fmt.Println("nothing matched; this is the default")
+	}
+}
