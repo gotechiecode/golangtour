4b41306acea2778ed7f70fe5551f1e06d7153fd7
Tangellapalli, Phani <PTangellapal@mast.com>
2020-08-31 11:53:35 +0530

    appending huge data in the same created file

--- 11_ReadWriteFiles/01_writeToFile/main.go
+++ 11_ReadWriteFiles/01_writeToFile/main.go
@@ -7,9 +7,12 @@ import (
 )
 
 func main()  {
-
+	_, error := os.Create("testResults-v1.csv")
+	if error != nil {
+		return
+	}
 	c1 := Customer{custId: "1", name:"Ram", age:30, gender:"male", mEmpolyee:true}
-	c2 := Customer{custId:"2", name:"Raheem", age:29, gender:"female", mEmpolyee:false}
+	c2 := Customer{custId:"2", name:"Cathy", age:29, gender:"female", mEmpolyee:false}
 	customers := []Customer{c1, c2}
 	writeToPNFile(customers)
 	
@@ -24,7 +27,7 @@ type Customer struct {
 }
 
 func writeToPNFile(records []Customer) {
-	file, error := os.Create("testResults-v1.csv")
+	file, error := os.OpenFile("testResults-v1.csv", os.O_APPEND|os.O_WRONLY, 0600)
 	if error != nil {
 		return
 	}
